177fed2f8a508710315b9c32e58764a858a3fcee
Akitoye Michael <akitoyemichael8@gmail.com>
2025-10-10 11:21:24 +0100

    02_Procedure_OrdersPlacedToday.sql

--- SQLScripts/05_Procedures/02_Procedure_OrdersPlacedToday.sql
+++ SQLScripts/05_Procedures/02_Procedure_OrdersPlacedToday.sql
@@ -1,18 +1,24 @@
 USE GlobalGadgetsDB;
 GO
-
---Orders Placed Today
-CREATE PROCEDURE [dbo].[OrdersPlacedToday]
+--==================================================
+--Orders Placed Today===============================
+CREATE PROCEDURE	dbo.GetTodayOrdersByCustomer @customer_id INT
 AS
 BEGIN
-    -- Count the number of orders placed today
-    SELECT COUNT(*) AS OrdersPlacedToday
-    FROM [dbo].[Orders]
-    WHERE CAST(order_date AS DATE) = CAST(GETDATE() AS DATE);
+    SELECT
+		p.product_id,
+		p.product_name,
+		s.supplier_id,
+		o.order_date
+	FROM Orders o
+	JOIN OrderDetails od ON o.order_id = od.order_id
+	JOIN Products p ON od.product_id = p.product_id
+	JOIN Suppliers s ON p.supplier_id = s.supplier_id
+    WHERE
+		o.customer_id = @customer_id
+		AND CAST(order_date AS DATE) = CAST(GETDATE() AS DATE);
 END
 GO
+--EXEC
+EXEC dbo.GetTodayOrdersByCustomer @customer_id = 4
 
-SELECT* FROM Orders
-
---to executive the stored procedure; 
-EXEC [dbo].[OrdersPlacedToday];
